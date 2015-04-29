9e2885f2adbf6cca700fc856a17239b9ecb85aaa
David Ortinau <dave@rendr.io>
2015-04-28 19:42:22 -0500

    readme

---
+++ README.mk
@@ -0,0 +1,7 @@
+** How to Use Code Templates in Xamarin Studio
+
+To add a template in Xamarin Studio, open Preferences > Code Templates.
+
+Select Add, paste the template code into the Template Text panel, give it a shortcut and fill out any other fields you like.
+
+In a code file, start typing the shortcut and hit tab to insert the template.
