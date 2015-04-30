7e079262d85006d03c26b8410aac3d47e0afeea9
David Ortinau <dave@rendr.io>
2015-04-30 17:00:51 -0500

    added factory template

---
+++ general/Factory.cs
@@ -0,0 +1,10 @@
+public static class $factory_name$
+{
+  public static Func<$interface_name$> Create { get; set; }
+
+  // TODO implement in platform specific
+  // $factory_name$.Create = () => new ConcreteImplmentationOfInterface()
+
+  // TODO use create method in other code
+  // var myThing = $factory_name$.Create();
+}
